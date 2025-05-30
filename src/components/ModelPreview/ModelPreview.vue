81dbb689df2cd511366d927820c9be3efb356bd0
杨永鑫 <3271310698@qq.com>
2025-05-31 04:07:01 +0800

    commit

---
+++ src/components/ModelPreview/ModelPreview.vue
@@ -0,0 +1,52 @@
+<template>
+    <div :class="container"></div>
+</template>
+<script>
+import * as THREE from 'three';
+export default {
+    name: 'vue-model-preview',
+    props: {
+        container: {
+            type: String,
+            default: 'model-preview-container'
+        }
+    },
+    data () {
+        this.scene = null;
+        this.camera = null;
+        this.mesh = null;
+        this.renderer = null;
+        return {
+        };
+    },
+    methods: {
+        init () {
+            this.createScene(); // 创建场景
+            this.loadModel(); // 加载模型
+            this.createLight(); // 创建光源
+            this.createCamera(); // 创建相机
+            this.createRender(); // 创建渲染器
+            this.render(); // 渲染
+        },
+        createScene () {
+            this.scene = new THREE.Scene();
+        },
+        createLight () {
+
+        },
+        createCamera () {
+
+        },
+        createRender () {
+
+        },
+        render () {
+            this.renderer.render(this.scene, this.camera);
+            requestAnimationFrame(this.render); // 循环渲染
+        }
+    },
+    mounted () {
+    }
+};
+</script>
+<style></style>
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,29 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+  server: {
+    host: '0.0.0.0',
+    port: 80
   },
+  plugins: [vue(), vueJsx({})],
   build: {
-    //打包后文件目录
-    outDir: "es",
-    //压缩
-    minify: false,
+    lib: {
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: "vue-model-preview",
+      formats: ['es', 'umd']
+    },
     rollupOptions: {
-
+      // 请确保外部化那些你的库中不需要的依赖
+      external: ["vue"],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: "Vue"
+        }
+      }
     }
-  },
-  plugins: [vue()],
+  }
 });
